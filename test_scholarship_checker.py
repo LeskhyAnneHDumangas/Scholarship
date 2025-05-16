660191b28cbbe38d817d8819380e53b39afe9c9f
LeskhyAnneHDumangas <leskhyanne@gmail.com>
2025-05-16 21:15:04 +0800

    update all files

--- test_scholarship_checker.py
+++ test_scholarship_checker.py
@@ -5,13 +5,13 @@ from scholarship_checker import is_eligible_for_scholarship
 class TestScholarshipEligibility(unittest.TestCase):
 
     def test_eligible_student(self):
-        self.assertTrue(is_eligible_for_scholarship (3.8,15000))
+        self.assertTrue(is_eligible_for_scholarship (3.8, 15000))
 
     def test_login_success(self):
-        self.assertTrue(is_eligible_for_scholarship (3.8,15000))
+        self.assertTrue(is_eligible_for_scholarship (3.8, 15000))
 
     def test_login_fail(self):
-        self.assertFalse(is_eligible_for_scholarship (3.8,15000))
+        self.assertFalse(is_eligible_for_scholarship (3.8, 15000))
 
     def test_low_gpa(self):
         self.assertTrue(is_eligible_for_scholarship (3.5))
@@ -20,10 +20,10 @@ class TestScholarshipEligibility(unittest.TestCase):
         self.assertTrue(is_eligible_for_scholarship (20000))
 
     def test_low_gpa_high_income(self):
-        self.assertTrue(is_eligible_for_scholarship (3.5,20000))
+        self.assertTrue(is_eligible_for_scholarship (3.5, 20000))
 
     def test_exact_threshold(self):
-        self.assertTrue(is_eligible_for_scholarship (3.5,20000))
+        self.assertTrue(is_eligible_for_scholarship (3.5, 20000))
 
     def test_invalid_gpa(self):
         self.assertFalse(is_eligible_for_scholarship ("Invalid GPA"))
